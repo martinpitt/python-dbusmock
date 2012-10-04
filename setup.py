cb548de0c4b7b9465b5ad0ca3ede1183c79ff5d3
Martin Pitt <martin.pitt@ubuntu.com>
2012-10-04 15:58:06 +0200

    setup.py: Use README.rst as long description.

--- setup.py
+++ setup.py
@@ -2,25 +2,20 @@
 
 import setuptools
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setuptools.setup(
     name = 'python-dbusmock',
     version = '0.1',
     description = 'Mock D-Bus objects',
-
+    long_description = readme,
     author = 'Martin Pitt',
     author_email = 'martin.pitt@ubuntu.com',
     url = 'https://launchpad.net/python-dbusmock',
     license = 'LGPL 3+',
-
     py_modules = ['dbusmock'],
     test_suite = 'nose.collector',
-
-    long_description = '''With this program/Python library you can easily
-create mock objects on D-Bus.  This is useful for writing tests for
-software which talks to D-Bus services such as upower, systemd, ConsoleKit,
-gnome-session or others, and it is hard (or impossible without root
-privileges) to set the state of the real services to what you expect in
-your tests.''',
 
     classifiers = [
         'Programming Language :: Python',
