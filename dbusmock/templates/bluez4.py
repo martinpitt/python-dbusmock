15f605e41eb44461c8e820258808f2d09d754069
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2014-07-22 17:11:19 -0400

    Add template for BlueZ 4

---
+++ dbusmock/templates/bluez4.py
@@ -0,0 +1,436 @@
+# -*- coding: utf-8 -*-
+
+'''bluetoothd mock template
+
+This creates the expected methods and properties of the object manager
+org.bluez object (/), but no adapters or devices.
+
+This supports BlueZ 4 only.
+'''
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.  See http://www.gnu.org/copyleft/lgpl.html for the full text
+# of the license.
+
+__authors__ = ['Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>',
+               'Philip Withnall <philip.withnall@collabora.co.uk>']
+__copyright__ = '(c) 2013 Collabora Ltd.'
+__copyright__ = '(c) 2014 Canonical Ltd.'
+__license__ = 'LGPL 3+'
+
+import dbus
+
+from dbusmock import OBJECT_MANAGER_IFACE, mockobject
+
+BUS_NAME = 'org.bluez'
+MAIN_OBJ = '/'
+SYSTEM_BUS = True
+IS_OBJECT_MANAGER = True
+
+BLUEZ_MOCK_IFACE = 'org.bluez.Mock'
+AGENT_MANAGER_IFACE = 'org.bluez.Agent'
+MANAGER_IFACE = 'org.bluez.Manager'
+ADAPTER_IFACE = 'org.bluez.Adapter'
+DEVICE_IFACE = 'org.bluez.Device'
+AUDIO_IFACE = 'org.bluez.Audio'
+
+
+def load(mock, parameters):
+    mock.AddObject('/org/bluez', AGENT_MANAGER_IFACE, {}, [
+        ('Release', '', '', ''),
+    ])
+
+    mock.AddMethods(MANAGER_IFACE, [
+        ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("org.bluez.Manager")'),
+        ('SetProperty', 'sv', '', 'self.Set("org.bluez.Manager", args[0], args[1]); '
+                                  'self.EmitSignal("org.bluez.Manager", "PropertyChanged", "sv", [args[0], args[1]])'),
+    ])
+    mock.AddProperties(MANAGER_IFACE, {
+        'Adapters': dbus.Array([], signature='o'),
+    })
+
+
+@dbus.service.method(BLUEZ_MOCK_IFACE,
+                     in_signature='ss', out_signature='s')
+def AddAdapter(self, device_name, system_name):
+    '''Convenience method to add a Bluetooth adapter
+
+    You have to specify a device name which must be a valid part of an object
+    path, e. g. "hci0", and an arbitrary system name (pretty hostname).
+
+    Returns the new object path.
+    '''
+    path = '/org/bluez/' + device_name
+    adapter_properties = {
+        'UUIDs': dbus.Array([
+            '00001000-0000-1000-8000-00805f9b34fb',
+            '00001001-0000-1000-8000-00805f9b34fb',
+            '0000112d-0000-1000-8000-00805f9b34fb',
+            '00001112-0000-1000-8000-00805f9b34fb',
+            '0000111f-0000-1000-8000-00805f9b34fb',
+            '0000111e-0000-1000-8000-00805f9b34fb',
+            '0000110c-0000-1000-8000-00805f9b34fb',
+            '0000110e-0000-1000-8000-00805f9b34fb',
+            '0000110a-0000-1000-8000-00805f9b34fb',
+            '0000110b-0000-1000-8000-00805f9b34fb',
+        ], variant_level=1),
+        'Discoverable': dbus.Boolean(False, variant_level=1),
+        'Discovering': dbus.Boolean(False, variant_level=1),
+        'Pairable': dbus.Boolean(True, variant_level=1),
+        'Powered': dbus.Boolean(True, variant_level=1),
+        'Address': dbus.String('00:01:02:03:04:05', variant_level=1),
+        'Alias': dbus.String(system_name, variant_level=1),
+        'Name': dbus.String(system_name, variant_level=1),
+        # Reference:
+        # http://bluetooth-pentest.narod.ru/software/
+        # bluetooth_class_of_device-service_generator.html
+        'Class': dbus.UInt32(268, variant_level=1),  # Computer, Laptop
+    }
+
+    self.AddObject(path,
+                   ADAPTER_IFACE,
+                   # Properties
+                   adapter_properties,
+                   # Methods
+                   [
+                       ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("org.bluez.Adapter")'),
+                       ('SetProperty', 'sv', '', 'self.Set("org.bluez.Adapter", args[0], args[1]); '
+                                                 'self.EmitSignal("org.bluez.Adapter", "PropertyChanged", "sv", [args[0], args[1]])'),
+                   ])
+
+    manager = mockobject.objects['/']
+    manager.props[MANAGER_IFACE]['Adapters'] \
+        = [dbus.ObjectPath(path, variant_level=1)]
+    manager.EmitSignal(OBJECT_MANAGER_IFACE, 'InterfacesAdded',
+                       'oa{sa{sv}}', [
+                           dbus.ObjectPath(path, variant_level=1),
+                           {ADAPTER_IFACE: adapter_properties},
+                       ])
+
+    manager.EmitSignal(MANAGER_IFACE, 'AdapterAdded',
+                       'o', [dbus.ObjectPath(path, variant_level=1)])
+    manager.EmitSignal(MANAGER_IFACE, 'DefaultAdapterChanged',
+                       'o', [dbus.ObjectPath(path, variant_level=1)])
+    manager.EmitSignal(MANAGER_IFACE, 'PropertyChanged', 'sv', [
+        "Adapters",
+        dbus.Array([dbus.ObjectPath(path, variant_level=1), ], variant_level=1),
+    ])
+
+    return path
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='', out_signature='')
+def StartDiscovery(self):
+    '''Start discovery
+    '''
+    adapter = mockobject.objects[self.path]
+
+    adapter.EmitSignal(ADAPTER_IFACE, 'PropertyChanged', 'sv', [
+        'Discovering', dbus.Boolean(True, variant_level=1),
+    ])
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='', out_signature='')
+def StopDiscovery(self):
+    '''Stop discovery
+    '''
+    adapter = mockobject.objects[self.path]
+
+    adapter.EmitSignal(dbus.PROPERTIES_IFACE, 'PropertiesChanged', 'sa{sv}as', [
+        ADAPTER_IFACE,
+        {
+            'Discovering': dbus.Boolean(False, variant_level=1),
+        },
+        [],
+    ])
+    adapter.EmitSignal(ADAPTER_IFACE, 'PropertyChanged', 'sv', [
+        'Discovering', dbus.Boolean(False, variant_level=1),
+    ])
+
+
+@dbus.service.method(MANAGER_IFACE,
+                     in_signature='', out_signature='o')
+def DefaultAdapter(self):
+    '''Retrieve the default adapter
+    '''
+    default_adapter = None
+
+    for obj in mockobject.objects.keys():
+        if obj.startswith('/org/bluez/') and 'dev_' not in obj:
+            default_adapter = obj
+
+    if default_adapter:
+        return dbus.ObjectPath(default_adapter, variant_level=1)
+    else:
+        raise dbus.exceptions.DBusException('org.bluez.Error.NoSuchAdapter',
+                                            'No such adapter.')
+
+
+@dbus.service.method(MANAGER_IFACE,
+                     in_signature='', out_signature='ao')
+def ListAdapters(self):
+    '''List all known adapters
+    '''
+    adapters = []
+
+    for obj in mockobject.objects.keys():
+        if obj.startswith('/org/bluez/') and 'dev_' not in obj:
+            adapters.append(dbus.ObjectPath(obj, variant_level=1))
+
+    return dbus.Array(adapters, variant_level=1)
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='s', out_signature='o')
+def CreateDevice(self, device_address):
+    '''Create a new device '''
+    device_name = 'dev_' + device_address.replace(':', '_').upper()
+    adapter_path = self.path
+    path = adapter_path + '/' + device_name
+
+    if path not in mockobject.objects:
+        raise dbus.exceptions.DBusException('org.bluez.Error.Failed',
+                                            'Could not create device for %s.' %
+                                            device_address)
+
+    adapter = mockobject.objects[self.path]
+    adapter.EmitSignal(ADAPTER_IFACE, 'DeviceCreated',
+                       'o', [dbus.ObjectPath(path, variant_level=1)])
+
+    return dbus.ObjectPath(path, variant_level=1)
+
+
+@dbus.service.method(BLUEZ_MOCK_IFACE,
+                     in_signature='sss', out_signature='s')
+def AddDevice(self, adapter_device_name, device_address, alias):
+    '''Convenience method to add a Bluetooth device
+
+    You have to specify a device address which must be a valid Bluetooth
+    address (e.g. 'AA:BB:CC:DD:EE:FF'). The alias is the human-readable name
+    for the device (e.g. as set on the device itself), and the adapter device
+    name is the device_name passed to AddAdapter.
+
+    This will create a new, unpaired and unconnected device.
+
+    Returns the new object path.
+    '''
+    device_name = 'dev_' + device_address.replace(':', '_').upper()
+    adapter_path = '/org/bluez/' + adapter_device_name
+    path = adapter_path + '/' + device_name
+
+    if adapter_path not in mockobject.objects:
+        raise dbus.exceptions.DBusException('org.bluez.Error.NoSuchAdapter',
+                                            'No such adapter.')
+
+    properties = {
+        'UUIDs': dbus.Array([], signature='s', variant_level=1),
+        'Blocked': dbus.Boolean(False, variant_level=1),
+        'Connected': dbus.Boolean(False, variant_level=1),
+        'LegacyPairing': dbus.Boolean(False, variant_level=1),
+        'Paired': dbus.Boolean(False, variant_level=1),
+        'Trusted': dbus.Boolean(False, variant_level=1),
+        'RSSI': dbus.Int16(-79, variant_level=1),  # arbitrary
+        'Adapter': dbus.ObjectPath(adapter_path, variant_level=1),
+        'Address': dbus.String(device_address, variant_level=1),
+        'Alias': dbus.String(alias, variant_level=1),
+        'Name': dbus.String(alias, variant_level=1),
+        'Class': dbus.UInt32(0x240404, variant_level=1),  # Audio, headset.
+        'Icon': dbus.String('audio-headset', variant_level=1),
+    }
+
+    self.AddObject(path,
+                   DEVICE_IFACE,
+                   # Properties
+                   properties,
+                   # Methods
+                   [
+                       ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("org.bluez.Device")'),
+                       ('SetProperty', 'sv', '', 'self.Set("org.bluez.Device", args[0], args[1]); '
+                                                 'self.EmitSignal("org.bluez.Device", "PropertyChanged", "sv", [args[0], args[1]])'),
+                   ])
+
+    manager = mockobject.objects['/']
+    manager.EmitSignal(OBJECT_MANAGER_IFACE, 'InterfacesAdded',
+                       'oa{sa{sv}}', [
+                           dbus.ObjectPath(path, variant_level=1),
+                           {DEVICE_IFACE: properties},
+                       ])
+
+    adapter = mockobject.objects[adapter_path]
+    adapter.EmitSignal(ADAPTER_IFACE, 'DeviceFound',
+                       'sa{sv}', [
+                           properties['Address'],
+                           properties,
+                       ])
+
+    return path
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='', out_signature='ao')
+def ListDevices(self):
+    '''List all known devices
+    '''
+    devices = []
+
+    for obj in mockobject.objects.keys():
+        if obj.startswith('/org/bluez/') and 'dev_' in obj:
+            devices.append(dbus.ObjectPath(obj, variant_level=1))
+
+    return dbus.Array(devices, variant_level=1)
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='s', out_signature='o')
+def FindDevice(self, address):
+    '''Find a specific device by bluetooth address.
+    '''
+    for obj in mockobject.objects.keys():
+        if obj.startswith('/org/bluez/') and 'dev_' in obj:
+            o = mockobject.objects[obj]
+            if o.props[DEVICE_IFACE]['Address'] \
+                    == dbus.String(address, variant_level=1):
+                return obj
+
+    raise dbus.exceptions.DBusException('org.bluez.Error.NoSuchDevice',
+                                        'No such device.')
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='sos', out_signature='o')
+def CreatePairedDevice(self, device_address, agent, capability):
+    '''Convenience method to mark an existing device as paired.
+    '''
+    device_name = 'dev_' + device_address.replace(':', '_').upper()
+    device_path = DefaultAdapter(self) + '/' + device_name
+
+    if device_path not in mockobject.objects:
+        raise dbus.exceptions.DBusException('org.bluez.Error.NoSuchDevice',
+                                            'No such device.')
+
+    device = mockobject.objects[device_path]
+
+    # Based off pairing with a Sennheise headset.
+    uuids = [
+        '00001108-0000-1000-8000-00805f9b34fb',
+        '0000110b-0000-1000-8000-00805f9b34fb',
+        '0000110e-0000-1000-8000-00805f9b34fb',
+        '0000111e-0000-1000-8000-00805f9b34fb',
+    ]
+
+    device.props[DEVICE_IFACE]['UUIDs'] = dbus.Array(uuids, variant_level=1)
+    device.props[DEVICE_IFACE]['Paired'] = dbus.Boolean(True, variant_level=1)
+    device.props[DEVICE_IFACE]['LegacyPairing'] = dbus.Boolean(True,
+                                                               variant_level=1)
+    device.props[DEVICE_IFACE]['Trusted'] = dbus.Boolean(False,
+                                                         variant_level=1)
+    device.props[DEVICE_IFACE]['Connected'] = dbus.Boolean(True,
+                                                           variant_level=1)
+
+    adapter = mockobject.objects[self.path]
+    adapter.EmitSignal(ADAPTER_IFACE, 'DeviceCreated',
+                       'o', [dbus.ObjectPath(device_path, variant_level=1)])
+
+    for prop in device.props[DEVICE_IFACE]:
+        try:
+            device.EmitSignal(DEVICE_IFACE, 'PropertyChanged', 'sv', [
+                prop, device.props[prop]
+            ])
+        except KeyError:
+            pass
+
+    return dbus.ObjectPath(device_path, variant_level=1)
+
+
+@dbus.service.method(DEVICE_IFACE,
+                     in_signature='s', out_signature='a{us}')
+def DiscoverServices(self, pattern):
+
+    device = mockobject.objects[self.path]
+
+    try:
+        device.AddProperties(AUDIO_IFACE,
+                             {
+                                 'State': dbus.String('disconnected', variant_level=1),
+                             })
+    except:
+        pass
+
+    device.props[AUDIO_IFACE]['State'] = dbus.String("disconnected",
+                                                     variant_level=1)
+
+    device.AddMethods(AUDIO_IFACE, [
+        ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("org.bluez.Audio")'),
+        ('SetProperty', 'sv', '', 'self.Set("org.bluez.Audio", args[0], args[1]); '
+                                  'self.EmitSignal("org.bluez.Audio", "PropertyChanged", "sv", [args[0], args[1]])'),
+    ])
+
+    return dbus.Dictionary({0: "dummy", }, variant_level=1)
+
+
+@dbus.service.method(AUDIO_IFACE,
+                     in_signature='', out_signature='')
+def Connect(self):
+    '''Connect a device '''
+    device_path = self.path
+
+    if device_path not in mockobject.objects:
+        raise dbus.exceptions.DBusException('org.bluez.Error.NoSuchDevice',
+                                            'No such device.')
+
+    device = mockobject.objects[device_path]
+
+    device.props[AUDIO_IFACE]['State'] = dbus.String("connected",
+                                                     variant_level=1)
+    device.EmitSignal(AUDIO_IFACE, 'PropertyChanged', 'sv', [
+        'State', dbus.String("connected", variant_level=1),
+    ])
+
+    device.props[DEVICE_IFACE]['Connected'] = dbus.Boolean(True,
+                                                           variant_level=1)
+    device.EmitSignal(DEVICE_IFACE, 'PropertyChanged', 'sv', [
+        'Connected', dbus.Boolean(True, variant_level=1),
+    ])
+
+
+@dbus.service.method(AUDIO_IFACE,
+                     in_signature='', out_signature='')
+def Disconnect(self):
+    '''Disconnect a device '''
+    device_path = self.path
+
+    if device_path not in mockobject.objects:
+        raise dbus.exceptions.DBusException('org.bluez.Error.NoSuchDevice',
+                                            'No such device.')
+
+    device = mockobject.objects[device_path]
+
+    try:
+        device.props[AUDIO_IFACE]['State'] = dbus.String("disconnected",
+                                                         variant_level=1)
+
+        device.EmitSignal(AUDIO_IFACE, 'PropertyChanged', 'sv', [
+            'State', dbus.String("disconnected", variant_level=1),
+        ])
+    except KeyError:
+        pass
+
+    device.props[DEVICE_IFACE]['Connected'] = dbus.Boolean(False,
+                                                           variant_level=1)
+    device.EmitSignal(DEVICE_IFACE, 'PropertyChanged', 'sv', [
+        'Connected', dbus.Boolean(False, variant_level=1),
+    ])
+
+
+@dbus.service.method(ADAPTER_IFACE,
+                     in_signature='o', out_signature='')
+def RemoveDevice(self, object_path):
+    '''Remove (forget) a device '''
+
+    adapter = mockobject.objects[self.path]
+    adapter.EmitSignal(ADAPTER_IFACE, 'DeviceRemoved',
+                       'o', [object_path])
---
+++ tests/test_bluez4.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.  See http://www.gnu.org/copyleft/lgpl.html for the full text
+# of the license.
+
+__authors__ = ['Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>',
+               'Philip Withnall <philip.withnall@collabora.co.uk>']
+__copyright__ = '(c) 2013 Collabora Ltd.'
+__copyright__ = '(c) 2014 Canonical Ltd.'
+__license__ = 'LGPL 3+'
+
+import unittest
+import sys
+import subprocess
+
+import dbus
+import dbus.mainloop.glib
+
+import dbusmock
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+p = subprocess.Popen(['which', 'bluez-test-adapter'], stdout=subprocess.PIPE)
+p.communicate()
+have_test_adapter = (p.returncode == 0)
+
+p = subprocess.Popen(['which', 'bluez-test-device'], stdout=subprocess.PIPE)
+p.communicate()
+have_test_device = (p.returncode == 0)
+
+
+def _run_test_command(prog, command):
+    '''Run bluez-test command with the given command.
+
+    Return its output as a list of lines.
+
+    If bluez-test-adapter returns a non-zero exit code, raise an Exception.
+    '''
+    cmd = []
+    cmd.append(prog)
+
+    if type(command) is str:
+        cmd.append(command)
+    else:
+        cmd.extend(command)
+
+    process = subprocess.Popen(cmd, stdin=None,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True)
+
+    out, err = process.communicate()
+
+    lines = out.split('\n')
+    lines = filter(lambda l: l != '', lines)
+    lines = list(lines)
+
+    errlines = err.split('\n')
+    errlines = filter(lambda l: l != '', errlines)
+    errlines = list(errlines)
+
+    return (lines, errlines)
+
+
+@unittest.skipUnless(have_test_adapter and have_test_device,
+                     'bluez 4 not installed')
+class TestBlueZ(dbusmock.DBusTestCase):
+    '''Test mocking bluetoothd'''
+
+    @classmethod
+    def setUpClass(klass):
+        klass.start_system_bus()
+        klass.dbus_con = klass.get_dbus(True)
+        (klass.p_mock, klass.obj_bluez) = klass.spawn_server_template(
+            'bluez4', {}, stdout=subprocess.PIPE)
+
+    def setUp(self):
+        self.obj_bluez.Reset()
+        self.dbusmock = dbus.Interface(self.obj_bluez, dbusmock.MOCK_IFACE)
+        self.dbusmock_bluez = dbus.Interface(self.obj_bluez, 'org.bluez.Mock')
+
+    def test_no_adapters(self):
+        # Check for adapters.
+        out, err = _run_test_command('bluez-test-adapter', 'list')
+        expected = "dbus.exceptions.DBusException: " \
+            + "org.freedesktop.DBus.Python.dbus.exceptions.DBusException: " \
+            + "('org.bluez.Error.NoSuchAdapter', 'No such adapter.')"
+        self.assertIn(expected, err)
+
+    def test_one_adapter(self):
+        # Chosen parameters.
+        adapter_name = 'hci0'
+        system_name = 'my-computer'
+
+        # Add an adapter
+        path = self.dbusmock_bluez.AddAdapter(adapter_name, system_name)
+        self.assertEqual(path, '/org/bluez/' + adapter_name)
+
+        adapter = self.dbus_con.get_object('org.bluez', path)
+        address = adapter.Get('org.bluez.Adapter', 'Address')
+        self.assertEqual(str(address), '00:01:02:03:04:05')
+
+        # Check for the adapter.
+        out, err = _run_test_command('bluez-test-adapter', 'list')
+        self.assertIn('    Name = ' + system_name, out)
+        self.assertIn('    Alias = ' + system_name, out)
+        self.assertIn('    Powered = 1', out)
+        self.assertIn('    Pairable = 1', out)
+        self.assertIn('    Discovering = 0', out)
+
+    def test_no_devices(self):
+        # Add an adapter.
+        adapter_name = 'hci0'
+        path = self.dbusmock_bluez.AddAdapter(adapter_name, 'my-computer')
+        self.assertEqual(path, '/org/bluez/' + adapter_name)
+
+        # Check for devices.
+        out, err = _run_test_command('bluez-test-device', 'list')
+        self.assertListEqual([], out)
+
+    def test_one_device(self):
+        # Add an adapter.
+        adapter_name = 'hci0'
+        path = self.dbusmock_bluez.AddAdapter(adapter_name, 'my-computer')
+        self.assertEqual(path, '/org/bluez/' + adapter_name)
+
+        # Add a device.
+        address = '11:22:33:44:55:66'
+        alias = 'My Phone'
+
+        path = self.dbusmock_bluez.AddDevice(adapter_name, address, alias)
+        self.assertEqual(path,
+                         '/org/bluez/' + adapter_name + '/dev_' +
+                         address.replace(':', '_'))
+
+        # Check for the device.
+        out, err = _run_test_command('bluez-test-device', 'list')
+        self.assertIn(address + ' ' + alias, out)
+
+        out, err = _run_test_command('bluez-test-device', ['name', address])
+        self.assertIn(alias, out)
+
+        device = self.dbus_con.get_object('org.bluez', path)
+        dev_name = device.Get('org.bluez.Device', 'Name')
+        self.assertEqual(str(dev_name), alias)
+        dev_address = device.Get('org.bluez.Device', 'Address')
+        self.assertEqual(str(dev_address), address)
+        dev_class = device.Get('org.bluez.Device', 'Class')
+        self.assertNotEqual(dev_class, 0)
+        dev_conn = device.Get('org.bluez.Device', 'Connected')
+        self.assertEqual(dev_conn, 0)
+
+    def test_connect_disconnect(self):
+        # Add an adapter.
+        adapter_name = 'hci0'
+        path = self.dbusmock_bluez.AddAdapter(adapter_name, 'my-computer')
+        self.assertEqual(path, '/org/bluez/' + adapter_name)
+
+        # Add a device.
+        address = '11:22:33:44:55:66'
+        alias = 'My Phone'
+
+        path = self.dbusmock_bluez.AddDevice(adapter_name, address, alias)
+        self.assertEqual(path,
+                         '/org/bluez/' + adapter_name + '/dev_' +
+                         address.replace(':', '_'))
+
+        # Force a service discovery so the audio interface appears.
+        device = self.dbus_con.get_object('org.bluez', path)
+        bluez_dev = dbus.Interface(device, 'org.bluez.Device')
+        bluez_dev.DiscoverServices("")
+
+        # Test the connection prior
+        dev_state = device.Get('org.bluez.Audio', 'State')
+        self.assertEqual(str(dev_state), "disconnected")
+        dev_conn = device.Get('org.bluez.Device', 'Connected')
+        self.assertEqual(dev_conn, 0)
+
+        # Connect the audio interface.
+        bluez_audio = dbus.Interface(device, 'org.bluez.Audio')
+        bluez_audio.Connect()
+
+        # Test the connection after connecting
+        dev_state = device.Get('org.bluez.Audio', 'State')
+        self.assertEqual(str(dev_state), "connected")
+        dev_conn = device.Get('org.bluez.Device', 'Connected')
+        self.assertEqual(dev_conn, 1)
+
+        # Disconnect audio
+        bluez_audio.Disconnect()
+
+        # Test the connection after connecting
+        dev_state = device.Get('org.bluez.Audio', 'State')
+        self.assertEqual(str(dev_state), "disconnected")
+        dev_conn = device.Get('org.bluez.Device', 'Connected')
+        self.assertEqual(dev_conn, 0)
+
+
+if __name__ == '__main__':
+    # avoid writing to stderr
+    unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout,
+                                                     verbosity=2))
