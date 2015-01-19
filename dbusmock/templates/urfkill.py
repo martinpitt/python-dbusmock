ca4954f3a02d901cf05ed288631e1eaface84e69
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2015-01-19 16:47:25 +0200

    Add template for urfkill

---
+++ dbusmock/templates/urfkill.py
@@ -0,0 +1,107 @@
+'''urfkill mock template
+
+This creates the expected methods and properties of the main
+urfkill object, but no devices. You can specify any property
+such as urfkill in "parameters".
+'''
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.  See http://www.gnu.org/copyleft/lgpl.html for the full text
+# of the license.
+
+__author__ = 'Jussi Pakkanen'
+__email__ = 'jussi.pakkanen@canonical.com'
+__copyright__ = '(C) 2015 Canonical ltd'
+__license__ = 'LGPL 3+'
+
+import dbus
+
+import dbusmock
+
+SYSTEM_BUS = True
+BUS_NAME = 'org.freedesktop.URfkill'
+MAIN_OBJ = '/org/freedesktop/URfkill'
+
+MAIN_IFACE = 'org.freedesktop.URfkill'
+
+individual_objects = ['BLUETOOTH', 'FM', 'GPS', 'NFC', 'UWB', 'WIMAX', 'WLAN', 'WWAN']
+type2objectname = {
+    1: 'WLAN',
+    2: 'BLUETOOTH',
+    3: 'UWB',
+    4: 'WIMAX',
+    5: 'WWAN',
+    6: 'GPS',
+    7: 'FM',
+}
+
+KS_NOTAVAILABLE = -1
+KS_UNBLOCKED = 0
+KS_SOFTBLOCKED = 1
+KS_HARDBLOCKED = 2
+
+
+def toggle_flight_mode(self, new_block_state):
+    new_block_state = bool(new_block_state)
+    if self.flight_mode == new_block_state:
+        return self.flight_mode
+    self.flight_mode = new_block_state
+    for i in individual_objects:
+        old_value = self.internal_states[i]
+        if old_value == 1:
+            continue  # It was already blocked so we don't need to do anything
+        path = '/org/freedesktop/URfkill/' + i
+        obj = dbusmock.get_object(path)
+        if new_block_state:
+            obj.Set('org.freedesktop.URfkill.Killswitch', 'state', 1)
+            obj.EmitSignal('org.freedesktop.URfkill.Killswitch', 'StateChanged', '', [])
+        else:
+            obj.Set('org.freedesktop.URfkill.Killswitch', 'state', 0)
+            obj.EmitSignal('org.freedesktop.URfkill.Killswitch', 'StateChanged', '', [])
+    self.EmitSignal(MAIN_IFACE, 'FlightModeChanged', 'b', [self.flight_mode])
+    return self.flight_mode
+
+
+def block(self, index, should_block):
+    should_block = bool(should_block)
+    if index not in type2objectname:
+        return False
+    objname = type2objectname[index]
+    if should_block:
+        new_block_state = 1
+    else:
+        new_block_state = 0
+    if self.internal_states[objname] != new_block_state:
+        path = '/org/freedesktop/URfkill/' + objname
+        obj = dbusmock.get_object(path)
+        self.internal_states[objname] = new_block_state
+        obj.Set('org.freedesktop.URfkill.Killswitch', 'state', new_block_state)
+        obj.EmitSignal('org.freedesktop.URfkill.Killswitch', 'StateChanged', '', [])
+
+
+def load(mock, parameters):
+    mock.toggle_flight_mode = toggle_flight_mode
+    mock.block = block
+    mock.flight_mode = False
+    mock.internal_states = {}
+    for oname in individual_objects:
+        mock.internal_states[oname] = KS_UNBLOCKED
+
+    # First we create the main urfkill object.
+    mock.AddMethods(MAIN_IFACE, [
+        ('IsFlightMode', '', 'b', 'ret = self.flight_mode'),
+        ('FlightMode', 'b', 'b', 'ret = self.toggle_flight_mode(self, args[0])'),
+        ('Block', 'ub', 'b', 'ret = self.block(self, args[0], args[1])'),
+    ])
+
+    mock.AddProperties(MAIN_IFACE,
+                       dbus.Dictionary({
+                           'DaemonVersion': parameters.get('DaemonVersion', '0.6.0'),
+                           'KeyControl': parameters.get('KeyControl', True)
+                       }, signature='sv'))
+
+    for i in individual_objects:
+        path = '/org/freedesktop/URfkill/' + i
+        mock.AddObject(path, 'org.freedesktop.URfkill.Killswitch', {'state': mock.internal_states[i]}, [])
---
+++ tests/test_urfkill.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.  See http://www.gnu.org/copyleft/lgpl.html for the full text
+# of the license.
+
+__author__ = 'Jussi Pakkanen'
+__email__ = 'jussi.pakkanen@canonical.com'
+__copyright__ = '(c) 2015 Canonical Ltd.'
+__license__ = 'LGPL 3+'
+
+import unittest
+import sys
+import subprocess
+import os
+import fcntl
+
+import dbus
+
+import dbusmock
+
+
+class TestURfkill(dbusmock.DBusTestCase):
+    '''Test mocked URfkill'''
+
+    @classmethod
+    def setUpClass(klass):
+        klass.start_system_bus()
+        klass.dbus_con = klass.get_dbus(True)
+
+    def setUp(self):
+        (self.p_mock, self.obj_urfkill) = self.spawn_server_template(
+            'urfkill', {},
+            stdout=subprocess.PIPE)
+        # set log to nonblocking
+        flags = fcntl.fcntl(self.p_mock.stdout, fcntl.F_GETFL)
+        fcntl.fcntl(self.p_mock.stdout, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+        self.dbusmock = dbus.Interface(self.obj_urfkill, dbusmock.MOCK_IFACE)
+
+    def tearDown(self):
+        self.p_mock.terminate()
+        self.p_mock.wait()
+
+    def get_urfkill_objects(self):
+        bus = dbus.SystemBus()
+        remote_object = bus.get_object('org.freedesktop.URfkill', '/org/freedesktop/URfkill')
+        iface = dbus.Interface(remote_object, 'org.freedesktop.URfkill')
+        return (remote_object, iface)
+
+    def test_mainobject(self):
+        (remote_object, iface) = self.get_urfkill_objects()
+        self.assertFalse(iface.IsFlightMode())
+        propiface = dbus.Interface(remote_object, 'org.freedesktop.DBus.Properties')
+        version = propiface.Get('org.freedesktop.URfkill', 'DaemonVersion')
+        self.assertEqual(version, '0.6.0')
+
+    def test_subobjects(self):
+        bus = dbus.SystemBus()
+        individual_objects = ['BLUETOOTH', 'FM', 'GPS', 'NFC', 'UWB', 'WIMAX', 'WLAN', 'WWAN']
+        for i in individual_objects:
+            path = '/org/freedesktop/URfkill/' + i
+            remote_object = bus.get_object('org.freedesktop.URfkill', path)
+            propiface = dbus.Interface(remote_object, 'org.freedesktop.DBus.Properties')
+            state = propiface.Get('org.freedesktop.URfkill.Killswitch', 'state')
+            self.assertEqual(state, 0)
+
+    def test_block(self):
+        bus = dbus.SystemBus()
+        (remote_object, iface) = self.get_urfkill_objects()
+
+        property_object = bus.get_object('org.freedesktop.URfkill', '/org/freedesktop/URfkill/WLAN')
+        propiface = dbus.Interface(property_object, 'org.freedesktop.DBus.Properties')
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 0)
+        iface.Block(1, True)
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 1)
+        iface.Block(1, False)
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 0)
+
+    def test_flightmode(self):
+        bus = dbus.SystemBus()
+        (remote_object, iface) = self.get_urfkill_objects()
+
+        property_object = bus.get_object('org.freedesktop.URfkill', '/org/freedesktop/URfkill/WLAN')
+        propiface = dbus.Interface(property_object, 'org.freedesktop.DBus.Properties')
+
+        self.assertFalse(iface.IsFlightMode())
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 0)
+        iface.FlightMode(True)
+        self.assertTrue(iface.IsFlightMode())
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 1)
+        iface.FlightMode(False)
+        self.assertFalse(iface.IsFlightMode())
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 0)
+
+    def test_flightmode_restore(self):
+        # An interface that was blocked remains blocked once flightmode is removed.
+        bus = dbus.SystemBus()
+        (remote_object, iface) = self.get_urfkill_objects()
+
+        property_object = bus.get_object('org.freedesktop.URfkill', '/org/freedesktop/URfkill/WLAN')
+        propiface = dbus.Interface(property_object, 'org.freedesktop.DBus.Properties')
+
+        self.assertFalse(iface.IsFlightMode())
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 0)
+        iface.Block(1, True)
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 1)
+        iface.FlightMode(True)
+        self.assertTrue(iface.IsFlightMode())
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 1)
+        iface.FlightMode(False)
+        self.assertFalse(iface.IsFlightMode())
+        self.assertEqual(propiface.Get('org.freedesktop.URfkill.Killswitch', 'state'), 1)
+
+
+if __name__ == '__main__':
+    # avoid writing to stderr
+    unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout, verbosity=2))
