c4675e8cd9e5b89e9e9212ec25606bdf94a3c9c4
Martin Pitt <martin@piware.de>
2018-02-22 21:07:11 +0100

    release 0.17.1

--- dbusmock/__init__.py
+++ dbusmock/__init__.py
@@ -11,7 +11,7 @@ __author__ = 'Martin Pitt'
 __email__ = 'martin.pitt@ubuntu.com'
 __copyright__ = '(c) 2012 Canonical Ltd.'
 __license__ = 'LGPL 3+'
-__version__ = '0.17'
+__version__ = '0.17.1'
 
 from dbusmock.mockobject import (DBusMockObject, MOCK_IFACE,
                                  OBJECT_MANAGER_IFACE, get_object, get_objects)
