c90ae0f0c8488c3749351e07aea7e75468e71602
Martin Pitt <martin@piware.de>
2022-07-16 22:03:30 +0200

    setup.py: Bump release to 0.28.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', encoding="UTF-8") as f:
 
 setuptools.setup(
     name='python-dbusmock',
-    version="0.28.1",
+    version="0.28.2",
     description='Mock D-Bus objects',
     long_description=readme,
     long_description_content_type='text/markdown',
