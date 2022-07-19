martinpitt/python-dbusmock

commit 872a7e332050c90ce594acbeb4de6d14216651a3

Move to f-strings
These have been around long enough now.

Fixes pylint C0209

dbusmock/templates/ofono.py
@@ -115,7 +115,7 @@ def AddModem(self, name, _properties):
 # Generate a new modem serial number so each modem we add gets a unique one.
 # Use a counter so that the result is predictable for tests.
 def new_modem_serial(mock):
-    serial = '12345678-1234-1234-1234-' + ('%012d' % mock.modem_serial_counter)
+    serial = f'12345678-1234-1234-1234-{mock.modem_serial_counter:012d}'
     mock.modem_serial_counter += 1
     return serial
 
@@ -123,7 +123,7 @@ def new_modem_serial(mock):
 # Generate a new unique IMSI (start with USA/AT&T 310/150 to match the MCC/MNC SIM properties)
 # Use a counter so that the result is predictable for tests.
 def new_imsi(mock):
-    imsi = '310150' + ('%09d' % mock.imsi_counter)
+    imsi = f'310150{mock.imsi_counter:09d}'
     mock.imsi_counter += 1
     return imsi
 
@@ -131,7 +131,7 @@ def new_imsi(mock):
 # Generate a new unique ICCID
 # Use a counter so that the result is predictable for tests.
 def new_iccid(mock):
-    iccid = '893581234' + ('%012d' % mock.iccid_counter)
+    iccid = f'893581234{mock.iccid_counter:012d}'
     mock.iccid_counter += 1
     return iccid
 
@@ -192,7 +192,7 @@ def add_voice_call_api(mock):
                      in_signature='ss', out_signature='s')
 def Dial(self, number, _hide_callerid):
     # pylint: disable=protected-access # _object_path
-    path = self._object_path + '/voicecall%02i' % (len(self.calls) + 1)
+    path = f'{self._object_path}/voicecall{(len(self.calls) + 1):02}'
     self.AddObject(path, 'org.ofono.VoiceCall',
                    {
                        'State': dbus.String('dialing', variant_level=1),
@@ -255,7 +255,7 @@ def HangupAll(self):
 
 def get_all_operators(mock):
     return 'ret = [(m, objects[m].GetAll("org.ofono.NetworkOperator")) ' \
-           'for m in objects if "%s/operator/" in m]' % mock.name
+           f'for m in objects if "{mock.name}/operator/" in m]'
 
 
 def add_netreg_api(mock):
@@ -276,7 +276,7 @@ def add_netreg_api(mock):
         'BaseStation': _parameters.get('BaseStation', ''),
     })
 
-    mock.AddObject('/%s/operator/op1' % mock.name,
+    mock.AddObject(f'/{mock.name}/operator/op1',
                    'org.ofono.NetworkOperator',
                    {
                        'Name': _parameters.get('Name', 'fake.tel'),
@@ -293,9 +293,8 @@ def add_netreg_api(mock):
 
     mock.AddMethods('org.ofono.NetworkRegistration', [
         ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("org.ofono.NetworkRegistration")'),
-        ('SetProperty', 'sv', '', 'self.Set("%(i)s", args[0], args[1]); '
-         'self.EmitSignal("%(i)s", "PropertyChanged", "sv", [args[0], args[1]])' % {
-             'i': 'org.ofono.NetworkRegistration'}),
+        ('SetProperty', 'sv', '', 'self.Set("org.ofono.NetworkRegistration", args[0], args[1]); '
+         'self.EmitSignal("org.ofono.NetworkRegistration", "PropertyChanged", "sv", [args[0], args[1]])'),
         ('Register', '', '', ''),
         ('GetOperators', '', 'a(oa{sv})', get_all_operators(mock)),
         ('Scan', '', 'a(oa{sv})', get_all_operators(mock)),
@@ -345,43 +344,45 @@ def add_simmanager_api(self, mock):
         'SubscriberIdentity': _parameters.get('SubscriberIdentity', new_imsi(self)),
     })
     mock.AddMethods(iface, [
-        ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("%s")' % iface),
-        ('SetProperty', 'sv', '', 'self.Set("%(i)s", args[0], args[1]); '
-         'self.EmitSignal("%(i)s", "PropertyChanged", "sv", [args[0], args[1]])' % {'i': iface}),
+        ('GetProperties', '', 'a{sv}', f'ret = self.GetAll("{iface}")'),
+        ('SetProperty', 'sv', '', f'self.Set("{iface}", args[0], args[1]); '
+         'self.EmitSignal("{iface}", "PropertyChanged", "sv", [args[0], args[1]])'),
         ('ChangePin', 'sss', '', ''),
 
         ('EnterPin', 'ss', '',
          'correctPin = "1234"\n'
-         'newRetries = self.Get("%(i)s", "Retries")\n'
+         f'iface = "{iface}"\n'
+         'newRetries = self.Get(iface, "Retries")\n'
          'if args[0] == "pin" and args[1] != correctPin:\n'
          '    newRetries["pin"] = dbus.Byte(newRetries["pin"] - 1)\n'
          'elif args[0] == "pin":\n'
          '    newRetries["pin"] = dbus.Byte(3)\n'
 
-         'self.Set("%(i)s", "Retries", newRetries)\n'
-         'self.EmitSignal("%(i)s", "PropertyChanged", "sv", ["Retries", newRetries])\n'
+         'self.Set(iface, "Retries", newRetries)\n'
+         'self.EmitSignal(iface, "PropertyChanged", "sv", ["Retries", newRetries])\n'
 
          'if args[0] == "pin" and args[1] != correctPin:\n'
          '    class Failed(dbus.exceptions.DBusException):\n'
          '        _dbus_error_name = "org.ofono.Error.Failed"\n'
-         '    raise Failed("Operation failed")' % {'i': iface}),
+         '    raise Failed("Operation failed")'),
 
         ('ResetPin', 'sss', '',
          'correctPuk = "12345678"\n'
-         'newRetries = self.Get("%(i)s", "Retries")\n'
+         f'iface = "{iface}"\n'
+         'newRetries = self.Get(iface, "Retries")\n'
          'if args[0] == "puk" and args[1] != correctPuk:\n'
          '    newRetries["puk"] = dbus.Byte(newRetries["puk"] - 1)\n'
          'elif args[0] == "puk":\n'
          '    newRetries["pin"] = dbus.Byte(3)\n'
          '    newRetries["puk"] = dbus.Byte(10)\n'
 
-         'self.Set("%(i)s", "Retries", newRetries)\n'
-         'self.EmitSignal("%(i)s", "PropertyChanged", "sv", ["Retries", newRetries])\n'
+         'self.Set(iface, "Retries", newRetries)\n'
+         'self.EmitSignal(iface, "PropertyChanged", "sv", ["Retries", newRetries])\n'
 
          'if args[0] == "puk" and args[1] != correctPuk:\n'
          '    class Failed(dbus.exceptions.DBusException):\n'
          '        _dbus_error_name = "org.ofono.Error.Failed"\n'
-         '    raise Failed("Operation failed")' % {'i': iface}),
+         '    raise Failed("Operation failed")'),
 
         ('LockPin', 'ss', '', ''),
         ('UnlockPin', 'ss', '', ''),
@@ -416,9 +417,9 @@ def add_connectionmanager_api(mock):
         'Powered': _parameters.get('ConnectionPowered', True),
     })
     mock.AddMethods(iface, [
-        ('GetProperties', '', 'a{sv}', 'ret = self.GetAll("%s")' % iface),
-        ('SetProperty', 'sv', '', 'self.Set("%(i)s", args[0], args[1]); '
-         'self.EmitSignal("%(i)s", "PropertyChanged", "sv", [args[0], args[1]])' % {'i': iface}),
+        ('GetProperties', '', 'a{sv}', f'ret = self.GetAll("{iface}")'),
+        ('SetProperty', 'sv', '', f'self.Set("{iface}", args[0], args[1]); '
+         'self.EmitSignal("{iface}", "PropertyChanged", "sv", [args[0], args[1]])'),
         ('AddContext', 's', 'o', 'ret = "/"'),
         ('RemoveContext', 'o', '', ''),
         ('DeactivateAll', '', '', ''),

dbusmock/__main__.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
         try:
             parameters = json.loads(args.parameters)
         except ValueError as detail:
-            sys.stderr.write('Malformed JSON given for parameters: %s\n' % detail)
+            sys.stderr.write(f'Malformed JSON given for parameters: {detail}\n')
             sys.exit(2)
 
         if not isinstance(parameters, dict):

dbusmock/mockobject.py
@@ -115,8 +115,7 @@ def _wrap_in_dbus_variant(value):
         return type(value)(value.conjugate(), variant_level=1)
     if isinstance(value, str):
         return dbus.String(value, variant_level=1)
-    raise dbus.exceptions.DBusException(
-        'could not wrap type {}'.format(type(value)))
+    raise dbus.exceptions.DBusException(f'could not wrap type {type(value)}')
 
 
 class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-attributes
@@ -178,7 +177,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
         if self.path == '/':
             cond = 'k != \'/\''
         else:
-            cond = 'k.startswith(\'%s/\')' % self.path
+            cond = f'k.startswith(\'{self.path}/\')'
 
         self.AddMethod(OBJECT_MANAGER_IFACE,
                        'GetManagedObjects', '', 'a{oa{sa{sv}}}',
@@ -201,7 +200,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
     def Get(self, interface_name: str, property_name: str) -> Any:
         '''Standard D-Bus API for getting a property value'''
 
-        self.log('Get %s.%s' % (interface_name, property_name))
+        self.log(f'Get {interface_name}.{property_name}')
 
         if not interface_name:
             interface_name = self.interface
@@ -233,9 +232,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
     def Set(self, interface_name: str, property_name: str, value: Any, *_, **__) -> None:
         '''Standard D-Bus API for setting a property value'''
 
-        self.log('Set %s.%s%s' % (interface_name,
-                                  property_name,
-                                  _format_args((value,))))
+        self.log(f'Set {interface_name}.{property_name}{_format_args((value,))}')
 
         try:
             iface_props = self.props[interface_name]
@@ -292,9 +289,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
                              ])
         '''
         if path in objects:
-            raise dbus.exceptions.DBusException(
-                'object %s already exists' % path,
-                name='org.freedesktop.DBus.Mock.NameError')
+            raise dbus.exceptions.DBusException(f'object {path} already exists', name='org.freedesktop.DBus.Mock.NameError')
 
         obj = DBusMockObject(self.bus_name,
                              path,
@@ -322,7 +317,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
             del objects[path]
         except KeyError as e:
             raise dbus.exceptions.DBusException(
-                'object %s does not exist' % path,
+                f'object {path} does not exist',
                 name='org.freedesktop.DBus.Mock.NameError') from e
 
     @dbus.service.method(MOCK_IFACE,
@@ -408,7 +403,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
                                           out_signature=out_sig)(method)
         dbus_method.__name__ = str(name)
         dbus_method._dbus_in_signature = in_sig
-        dbus_method._dbus_args = ['arg%i' % i for i in range(1, n_args + 1)]
+        dbus_method._dbus_args = [f'arg{i}' for i in range(1, n_args + 1)]
 
         # for convenience, add mocked methods on the primary interface as
         # callable methods
@@ -458,9 +453,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
             if not interface:
                 interface = self.interface
             if name not in self.props.get(interface, {}):
-                raise dbus.exceptions.DBusException(
-                    'property %s not found' % name,
-                    name=interface + '.NoSuchProperty')
+                raise dbus.exceptions.DBusException(f'property {name} not found', name=interface + '.NoSuchProperty')
 
             self._set_property(interface, name, value)
             changed_props[name] = _wrap_in_dbus_variant(value)
@@ -483,9 +476,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
         if not interface:
             interface = self.interface
         if name in self.props.get(interface, {}):
-            raise dbus.exceptions.DBusException(
-                'property %s already exists' % name,
-                name=self.interface + '.PropertyExists')
+            raise dbus.exceptions.DBusException(f'property {name} already exists', name=self.interface + '.PropertyExists')
 
         self._set_property(interface, name, value)
 
@@ -528,7 +519,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
         try:
             module = load_module(template)
         except ImportError as e:
-            raise dbus.exceptions.DBusException('Cannot add template %s: %s' % (template, str(e)),
+            raise dbus.exceptions.DBusException(f'Cannot add template {template}: {str(e)}',
                                                 name='org.freedesktop.DBus.Mock.TemplateError')
 
         # If the template specifies this is an ObjectManager, set that up
@@ -585,11 +576,11 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
         m.append(signature=signature, *args)
         args = m.get_args_list()
 
-        fn = lambda self, *args: self.log('emit %s.%s%s' % (interface, name, _format_args(args)))
+        fn = lambda self, *args: self.log(f'emit {interface}.{name}{_format_args(args)}')
         fn.__name__ = str(name)
         dbus_fn = dbus.service.signal(interface)(fn)
         dbus_fn._dbus_signature = signature
-        dbus_fn._dbus_args = ['arg%i' % i for i in range(1, len(args) + 1)]
+        dbus_fn._dbus_args = [f'arg{i}' for i in range(1, len(args) + 1)]
 
         dbus_fn(self, *args)
 
@@ -692,7 +683,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
         else:
             fd = sys.stdout.fileno()
 
-        os.write(fd, ('%.3f %s\n' % (time.time(), msg)).encode('UTF-8'))
+        os.write(fd, f'{time.time():.3f} {msg}\n'.encode('UTF-8'))
 
     @dbus.service.method(dbus.INTROSPECTABLE_IFACE,
                          in_signature='',
@@ -726,7 +717,7 @@ class DBusMockObject(dbus.service.Object):  # pylint: disable=too-many-instance-
             # We might have properties for new interfaces we don't know about
             # yet. Try to find an existing <interface> node named after our
             # interface to append to, and create one if we can't.
-            interface = tree.find(".//interface[@name='%s']" % name)
+            interface = tree.find(f".//interface[@name='{name}']")
             if interface is None:
                 interface = ElementTree.Element("interface", {"name": name})
                 tree.append(interface)

dbusmock/templates/accounts_service.py
@@ -27,7 +27,7 @@ DEFAULT_USER_PASSWORD = 'Pa$$wo0rd'
 
 
 def get_user_path(uid):
-    return '/org/freedesktop/Accounts/User{}'.format(uid)
+    return f'/org/freedesktop/Accounts/User{uid}'
 
 
 def load(mock, parameters=None):
@@ -74,9 +74,9 @@ def AddUser(self, uid, username, password=DEFAULT_USER_PASSWORD,
         'AccountType': dbus.Int32(1),
         'AutomaticLogin': False,
         'BackgroundFile': '',
-        'Email': '{}@python-dbusmock.org'.format(username),
+        'Email': f'{username}@python-dbusmock.org',
         'FormatsLocale': 'C',
-        'HomeDirectory': '/nonexisting/mock-home/{}'.format(username),
+        'HomeDirectory': f'/nonexisting/mock-home/{username}',
         'IconFile': '',
         'InputSources': dbus.Array([], signature='a{ss}'),
         'Language': 'C',
@@ -164,9 +164,7 @@ def CreateUser(self, name, fullname, account_type):
         found = False
 
     if found:
-        raise dbus.exceptions.DBusException(
-            'User {} already exists'.format(name),
-            name='org.freedesktop.Accounts.Error.Failed')
+        raise dbus.exceptions.DBusException(f'User {name} already exists', name='org.freedesktop.Accounts.Error.Failed')
 
     self.users_auto_uids += 1
 
@@ -231,9 +229,7 @@ def FindUserByName(self, username):
         [user_id] = [uid for uid, props in self.mock_users.items()
                      if props['UserName'] == username]
     except ValueError as e:
-        raise dbus.exceptions.DBusException(
-            'No such user exists: {}'.format(e),
-            name='org.freedesktop.Accounts.Error.Failed')
+        raise dbus.exceptions.DBusException(f'No such user exists: {e}', name='org.freedesktop.Accounts.Error.Failed')
     return get_user_path(user_id)
 
 

dbusmock/templates/bluez5.py
@@ -205,7 +205,7 @@ def AddDevice(self, adapter_device_name, device_address, alias):
 
     if adapter_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Adapter %s does not exist.' % adapter_device_name,
+            f'Adapter {adapter_device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchAdapter')
 
     properties = {
@@ -270,12 +270,10 @@ def PairDevice(_self, adapter_device_name, device_address, class_=5898764):
 
     if adapter_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Adapter %s does not exist.' % adapter_device_name,
+            f'Adapter {adapter_device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchAdapter')
     if device_path not in mockobject.objects:
-        raise dbus.exceptions.DBusException(
-            'Device %s does not exist.' % device_name,
-            name=BLUEZ_MOCK_IFACE + '.NoSuchDevice')
+        raise dbus.exceptions.DBusException(f'Device {device_name} does not exist.', name=BLUEZ_MOCK_IFACE + '.NoSuchDevice')
 
     device = mockobject.objects[device_path]
     device.paired = True
@@ -348,12 +346,10 @@ def BlockDevice(_self, adapter_device_name, device_address):
 
     if adapter_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Adapter %s does not exist.' % adapter_device_name,
+            f'Adapter {adapter_device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchAdapter')
     if device_path not in mockobject.objects:
-        raise dbus.exceptions.DBusException(
-            'Device %s does not exist.' % device_name,
-            name=BLUEZ_MOCK_IFACE + '.NoSuchDevice')
+        raise dbus.exceptions.DBusException(f'Device {device_name} does not exist.', name=BLUEZ_MOCK_IFACE + '.NoSuchDevice')
 
     device = mockobject.objects[device_path]
 
@@ -393,11 +389,11 @@ def ConnectDevice(_self, adapter_device_name, device_address):
 
     if adapter_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Adapter %s does not exist.' % adapter_device_name,
+            f'Adapter {adapter_device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchAdapter')
     if device_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Device %s does not exist.' % device_name,
+            f'Device {device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchDevice')
 
     device = mockobject.objects[device_path]
@@ -439,11 +435,11 @@ def DisconnectDevice(_self, adapter_device_name, device_address):
 
     if adapter_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Adapter %s does not exist.' % adapter_device_name,
+            f'Adapter {adapter_device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchAdapter')
     if device_path not in mockobject.objects:
         raise dbus.exceptions.DBusException(
-            'Device %s does not exist.' % device_name,
+            f'Device {device_name} does not exist.',
             name=BLUEZ_MOCK_IFACE + '.NoSuchDevice')
 
     device = mockobject.objects[device_path]

dbusmock/templates/iio-sensors-proxy.py
@@ -177,10 +177,10 @@ def SetInternalProperty(self, interface, property_name, value):
     owners = None
     if is_valid_sensor_for_interface(sensor, interface):
 
-        if not getattr(self, 'has_{}'.format(sensor)):
-            raise Exception('No {} sensor available'.format(sensor))
+        if not getattr(self, f'has_{sensor}'):
+            raise Exception(f'No {sensor} sensor available')
 
-        owners = getattr(self, '{}_owners'.format(sensor))
+        owners = getattr(self, f'{sensor}_owners')
         # We allow setting a property from any client here, even if not claiming
         # but only owners, if any, will be notified about sensors changes
 

dbusmock/templates/logind.py
@@ -34,12 +34,12 @@ def load(mock, parameters):
         ('Hibernate', 'b', '', ''),
         ('HybridSleep', 'b', '', ''),
         ('SuspendThenHibernate', 'b', '', ''),
-        ('CanPowerOff', '', 's', 'ret = "%s"' % parameters.get('CanPowerOff', 'yes')),
-        ('CanReboot', '', 's', 'ret = "%s"' % parameters.get('CanReboot', 'yes')),
-        ('CanSuspend', '', 's', 'ret = "%s"' % parameters.get('CanSuspend', 'yes')),
-        ('CanHibernate', '', 's', 'ret = "%s"' % parameters.get('CanHibernate', 'yes')),
-        ('CanHybridSleep', '', 's', 'ret = "%s"' % parameters.get('CanHybridSleep', 'yes')),
-        ('CanSuspendThenHibernate', '', 's', 'ret = "%s"' % parameters.get('CanSuspendThenHibernate', 'yes')),
+        ('CanPowerOff', '', 's', f'ret = "{parameters.get("CanPowerOff", "yes")}"'),
+        ('CanReboot', '', 's', f'ret = "{parameters.get("CanReboot", "yes")}"'),
+        ('CanSuspend', '', 's', f'ret = "{parameters.get("CanSuspend", "yes")}"'),
+        ('CanHibernate', '', 's', f'ret = "{parameters.get("CanHibernate", "yes")}"'),
+        ('CanHybridSleep', '', 's', f'ret = "{parameters.get("CanHybridSleep", "yes")}"'),
+        ('CanSuspendThenHibernate', '', 's', f'ret = "{parameters.get("CanSuspendThenHibernate", "yes")}"'),
 
         ('GetSession', 's', 'o', 'ret = "/org/freedesktop/login1/session/" + args[0]'),
         ('ActivateSession', 's', '', ''),
@@ -56,7 +56,7 @@ def load(mock, parameters):
         ('ListSeats', '', 'a(so)', 'ret = [(k.split("/")[-1], k) for k in objects.keys() if "/seat/" in k]'),
         ('TerminateSeat', 's', '', ''),
 
-        ('GetUser', 'u', 'o', 'ret = "/org/freedesktop/login1/user/%u" % args[0]'),
+        ('GetUser', 'u', 'o', 'ret = "/org/freedesktop/login1/user/" + args[0]'),
         ('KillUser', 'us', '', ''),
         ('TerminateUser', 'u', '', ''),
     ])
@@ -152,8 +152,7 @@ def AddSeat(self, seat):
     '''
     seat_path = '/org/freedesktop/login1/seat/' + seat
     if seat_path in mockobject.objects:
-        raise dbus.exceptions.DBusException('Seat %s already exists' % seat,
-                                            name=MOCK_IFACE + '.SeatExists')
+        raise dbus.exceptions.DBusException(f'Seat {seat} already exists', name=MOCK_IFACE + '.SeatExists')
 
     self.AddObject(seat_path,
                    'org.freedesktop.login1.Seat',
@@ -183,10 +182,9 @@ def AddUser(self, uid, username, active):
 
     Return the object path of the new user.
     '''
-    user_path = '/org/freedesktop/login1/user/%i' % uid
+    user_path = f'/org/freedesktop/login1/user/{uid}'
     if user_path in mockobject.objects:
-        raise dbus.exceptions.DBusException('User %i already exists' % uid,
-                                            name=MOCK_IFACE + '.UserExists')
+        raise dbus.exceptions.DBusException(f'User {uid} already exists', name=MOCK_IFACE + '.UserExists')
 
     self.AddObject(user_path,
                    'org.freedesktop.login1.User',
@@ -195,7 +193,7 @@ def AddUser(self, uid, username, active):
                        'IdleHint': False,
                        'DefaultControlGroup': 'systemd:/user/' + username,
                        'Name': username,
-                       'RuntimePath': '/run/user/%i' % uid,
+                       'RuntimePath': f'/run/user/{uid}',
                        'Service': '',
                        'State': (active and 'active' or 'online'),
                        'Display': ('', dbus.ObjectPath('/')),
@@ -223,17 +221,17 @@ def AddSession(self, session_id, seat, uid, username, active):
 
     Return the object path of the new session.
     '''
-    seat_path = dbus.ObjectPath('/org/freedesktop/login1/seat/' + seat)
+    seat_path = dbus.ObjectPath(f'/org/freedesktop/login1/seat/{seat}')
     if seat_path not in mockobject.objects:
         self.AddSeat(seat)
 
-    user_path = dbus.ObjectPath('/org/freedesktop/login1/user/%i' % uid)
+    user_path = dbus.ObjectPath(f'/org/freedesktop/login1/user/{uid}')
     if user_path not in mockobject.objects:
         self.AddUser(uid, username, active)
 
-    session_path = dbus.ObjectPath('/org/freedesktop/login1/session/' + session_id)
+    session_path = dbus.ObjectPath(f'/org/freedesktop/login1/session/{session_id}')
     if session_path in mockobject.objects:
-        raise dbus.exceptions.DBusException('Session %s already exists' % session_id,
+        raise dbus.exceptions.DBusException(f'Session {session_id} already exists',
                                             name=MOCK_IFACE + '.SessionExists')
 
     self.AddObject(session_path,
@@ -246,7 +244,7 @@ def AddSession(self, session_id, seat, uid, username, active):
                        'KillProcesses': False,
                        'Remote': False,
                        'Class': 'user',
-                       'DefaultControlGroup': 'systemd:/user/%s/%s' % (username, session_id),
+                       'DefaultControlGroup': f'systemd:/user/{username}/{session_id}',
                        'Display': os.getenv('DISPLAY', ''),
                        'Id': session_id,
                        'Name': username,

commit 301edbc5e44bf40947dd23d21260435070f8bc2c

ofono template: fix conversion to f-strings
For the continued Python string, the f-string identifier has to be
re-specified again. Without this, tests of lomiri-indicator-network
breaks.

Fixes: 872a7e33 ("Move to f-strings")

dbusmock/templates/ofono.py
@@ -346,7 +346,7 @@ def add_simmanager_api(self, mock):
     mock.AddMethods(iface, [
         ('GetProperties', '', 'a{sv}', f'ret = self.GetAll("{iface}")'),
         ('SetProperty', 'sv', '', f'self.Set("{iface}", args[0], args[1]); '
-         'self.EmitSignal("{iface}", "PropertyChanged", "sv", [args[0], args[1]])'),
+         f'self.EmitSignal("{iface}", "PropertyChanged", "sv", [args[0], args[1]])'),
         ('ChangePin', 'sss', '', ''),
 
         ('EnterPin', 'ss', '',
@@ -419,7 +419,7 @@ def add_connectionmanager_api(mock):
     mock.AddMethods(iface, [
         ('GetProperties', '', 'a{sv}', f'ret = self.GetAll("{iface}")'),
         ('SetProperty', 'sv', '', f'self.Set("{iface}", args[0], args[1]); '
-         'self.EmitSignal("{iface}", "PropertyChanged", "sv", [args[0], args[1]])'),
+         f'self.EmitSignal("{iface}", "PropertyChanged", "sv", [args[0], args[1]])'),
         ('AddContext', 's', 'o', 'ret = "/"'),
         ('RemoveContext', 'o', '', ''),
         ('DeactivateAll', '', '', ''),