b46214e03bb57dc7be8604d137b25b40d7482259
Martin Pitt <martin@piware.de>
2022-07-19 14:04:30 +0200

    Release 0.28.4

--- dbusmock/__init__.py
+++ dbusmock/__init__.py
@@ -9,7 +9,7 @@
 
 __author__ = 'Martin Pitt'
 __copyright__ = '(c) 2012 Canonical Ltd.'
-__version__ = '0.28.3'
+__version__ = '0.28.4'
 
 
 from dbusmock.mockobject import (DBusMockObject, MOCK_IFACE,
