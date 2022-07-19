martinpitt/python-dbusmock

commit 842d2506fb7184692e4907f8d5482c0da273ae48

Bring back dbusmock.__version__
autotools's version checks or upower's test suite rely on this, so this
is API. Add a unit test to ensure it exists.

This should ideally be generated automatically [1], but that does not
work as long as packit, rpm builds etc. don't get along with pyproject.

So for the time being, move the static version back to
dbusmock/__init__.py, and read it from setup.py instead.

[1] https://github.com/martinpitt/python-dbusmock/pull/140

setup.py
@@ -6,12 +6,14 @@
 
 import setuptools
 
+from dbusmock import __version__
+
 with open('README.md', encoding="UTF-8") as f:
     readme = f.read()
 
 setuptools.setup(
     name='python-dbusmock',
-    version="0.28.2",
+    version=__version__,
     description='Mock D-Bus objects',
     long_description=readme,
     long_description_content_type='text/markdown',

dbusmock/__init__.py
@@ -9,6 +9,8 @@
 
 __author__ = 'Martin Pitt'
 __copyright__ = '(c) 2012 Canonical Ltd.'
+__version__ = '0.28.2'
+
 
 from dbusmock.mockobject import (DBusMockObject, MOCK_IFACE,
                                  OBJECT_MANAGER_IFACE, get_object, get_objects)

tests/test_api.py
@@ -1073,6 +1073,9 @@ class TestServiceAutostart(dbusmock.DBusTestCase):
 
         dbus_if.StartServiceByName('org.TestSystem', 0)
 
+    def test_version(self):
+        self.assertGreater(dbusmock.__version__, "0.28.0")
+
 
 if __name__ == '__main__':
     # avoid writing to stderr

commit 8d464121a16e8df586375167bcdadc927f783878

setup.py: Fix ModuleNotFoundError
Commit 842d2506fb71 broke `pip install` and also some variants of
PEP-517 build tools: `.` is not in their default `sys.path` any more.
Merely adding that to the search path is not sufficient: building an
sdist in a venv is tripping over the missing `import dbus`, as
dbus-python is just a runtime dependency, not a build-time one.

So instead of importing `dbusmock.__version__` in setup.py, ask
setuptools to dynamically generate the version in a declarative way, in
setup.cfg.

Many thanks to Michał Górny for analyzing this and proposing this
solution!

Fixes #142

setup.cfg
@@ -1,0 +1,2 @@
+[metadata]
+version = attr: dbusmock.__version__

setup.py
@@ -6,14 +6,11 @@
 
 import setuptools
 
-from dbusmock import __version__
-
 with open('README.md', encoding="UTF-8") as f:
     readme = f.read()
 
 setuptools.setup(
     name='python-dbusmock',
-    version=__version__,
     description='Mock D-Bus objects',
     long_description=readme,
     long_description_content_type='text/markdown',