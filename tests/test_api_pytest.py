47adffe3fbb02cea303e84bc3782393f6d891259
Peter Hutterer <peter.hutterer@who-t.net>
2023-11-06 20:11:41 +1000

    testcase: factor the server spawning into a new SpawnedMock object
    Let's move spawn_server and spawn_server_template into an object with a
    context manager, greatly simplifying them where fixtures are being used.
    Note that both are renamed into:
     SpawnedMock.spawn_for_name()
     SpawedMock.spawn_with_template()

--- tests/test_api_pytest.py
+++ tests/test_api_pytest.py
@@ -34,7 +34,7 @@ def test_dbusmock_test_spawn_server(dbusmock_session):
 
 @pytest.fixture(name='upower_mock')
 def fixture_upower_mock(dbusmock_system):
-    p_mock, obj = dbusmock_system.spawn_server_template('upower', stdout=subprocess.DEVNULL)
+    p_mock, obj = dbusmock_system.spawn_with_template('upower', stdout=subprocess.DEVNULL)
     yield obj
     p_mock.terminate()
     p_mock.wait()
@@ -42,6 +42,30 @@ def fixture_upower_mock(dbusmock_system):
 
 def test_dbusmock_test_spawn_system_template(upower_mock):
     assert upower_mock
+    out = subprocess.check_output(['upower', '--dump'], universal_newlines=True)
+    assert 'version:' in out
+    assert '0.99' in out
+
+
+def test_dbusmock_test_spawnedserver(dbusmock_session):
+    assert dbusmock_session
+    test_iface = 'org.freedesktop.Test.Main'
+
+    with dbusmock.SpawnedMock.spawn_for_name('org.freedesktop.Test', '/', test_iface) as server:
+        obj_test = server.obj
+        obj_test.AddMethod('', 'Upper', 's', 's', 'ret = args[0].upper()', interface_name=dbusmock.MOCK_IFACE)
+        assert obj_test.Upper('hello', interface=test_iface) == 'HELLO'
+
+
+@pytest.fixture(name='upower_mock_spawned')
+def fixture_upower_mock_spawned(dbusmock_system):
+    assert dbusmock_system
+    with dbusmock.SpawnedMock.spawn_with_template('upower') as server:
+        yield server.obj
+
+
+def test_dbusmock_test_spawnedserver_template(upower_mock_spawned):
+    assert upower_mock_spawned
     out = subprocess.check_output(['upower', '--dump'], universal_newlines=True)
     assert 'version:' in out
     assert '0.99' in out
