6a3dafaf383699345e29e01aad5b020b1e0b6996
Peter Hutterer <peter.hutterer@who-t.net>
2023-11-29 15:30:52 +1000

    tests: skip pylint/ruff/mypy if not available
    Now that we're running it by default, let's be nice and skip them if
    they're not available locally.

--- tests/test_code.py
+++ tests/test_code.py
@@ -11,6 +11,7 @@ __copyright__ = '''
 '''
 
 import glob
+import importlib.util
 import os
 import subprocess
 import sys
@@ -19,6 +20,8 @@ import unittest
 
 @unittest.skipIf(os.getenv("SKIP_STATIC_CHECKS", "0") == "1", "$SKIP_STATIC_CHECKS set, not running static code checks")
 class StaticCodeTests(unittest.TestCase):
+
+    @unittest.skipUnless(importlib.util.find_spec("pylint"), "pylint not available, skipping")
     def test_pylint(self):
         subprocess.check_call([sys.executable, '-m', 'pylint', *glob.glob('dbusmock/*.py')])
         # signatures/arguments are not determined by us, docstrings are a bit pointless, and code repetition
@@ -33,11 +36,15 @@ class StaticCodeTests(unittest.TestCase):
                                '--disable=too-many-public-methods,too-many-lines,too-many-statements,R0801',
                                'tests/'])
 
+    @unittest.skipUnless(importlib.util.find_spec("mypy"), "mypy not available, skipping")
     def test_types(self):
         subprocess.check_call([sys.executable, '-m', 'mypy', 'dbusmock/', 'tests/'])
 
     def test_ruff(self):
-        subprocess.check_call(['ruff', 'check', '--no-cache', '.'])
+        try:
+            subprocess.check_call(['ruff', 'check', '--no-cache', '.'])
+        except FileNotFoundError:
+            self.skipTest("ruff not available, skipping")
 
 
 if __name__ == '__main__':
