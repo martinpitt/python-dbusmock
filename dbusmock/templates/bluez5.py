martinpitt/python-dbusmock

commit 63264e182dae27c7bc70e9afb6ab1fa5e206c56b

bluez5: Clean up static default properties, re-drop PairDevice class_ parameter
Over the years, this template has accumulated some hacks and bad API
which made PairDevice()'s handling of the Modalias/Class/Icon properties
buggy and hard to understand:

 * These are *static* device properties, they are not supposed to change
   during pairing.
 * Commit ee29a4403359b6a added these as some kind of "dynamic fallback
   default" when they were not initialized by the caller after
   AddDevice().
 * Commit 59d6af0dca3e silently broke that fallback default by changing
   AddDevice() to set these device properties to empty strings.
 * Commit fae4be7f49c0861 added another really bad API for setting Class
   in PairDevice()(). That API didn't fit into D-Bus (see commit
   8968284e8b which had to make it a non-default parameter) and also
   broke the API, and moreover it is totally unintuitive -- the device
   class has nothing to do with pairing.

Clean up all of these: Set the static property defaults in AddDevice()
right away, so that the caller can adjust them afterwards. Re-drop the
`class_` argument in PairDevice(). Adjust the documentation of
AddDevice() to point out that properties should be changed after calling
that.

Consequently, PairDevice() will stop claiming that the static properties
changed. This also gets rid of some redundant code.

dbusmock/templates/bluez5.py
@@ -383,7 +383,9 @@ def AddDevice(self, adapter_device_name, device_address, alias):
     for the device (e.g. as set on the device itself), and the adapter device
     name is the device_name passed to AddAdapter.
 
-    This will create a new, unpaired and unconnected device.
+    This will create a new, unpaired and unconnected device with some default properties
+    like MOCK_PHONE_CLASS "Class" and a static "Modalias". Especially when working with more
+    than one device, you may want to change these after creation.
 
     Returns the new object path.
     """
@@ -400,8 +402,8 @@ def AddDevice(self, adapter_device_name, device_address, alias):
         "Address": dbus.String(device_address, variant_level=1),
         "AddressType": dbus.String("public", variant_level=1),
         "Name": dbus.String(alias, variant_level=1),
-        "Icon": dbus.String("", variant_level=1),
-        "Class": dbus.UInt32(0, variant_level=1),
+        "Icon": dbus.String("phone", variant_level=1),
+        "Class": dbus.UInt32(MOCK_PHONE_CLASS, variant_level=1),
         "Appearance": dbus.UInt16(0, variant_level=1),
         "UUIDs": dbus.Array([], signature="s", variant_level=1),
         "Paired": dbus.Boolean(False, variant_level=1),
@@ -412,7 +414,7 @@ def AddDevice(self, adapter_device_name, device_address, alias):
         "Alias": dbus.String(alias, variant_level=1),
         "Adapter": dbus.ObjectPath(adapter_path, variant_level=1),
         "LegacyPairing": dbus.Boolean(False, variant_level=1),
-        "Modalias": dbus.String("", variant_level=1),
+        "Modalias": dbus.String("bluetooth:v000Fp1200d1436", variant_level=1),
         "RSSI": dbus.Int16(-79, variant_level=1),  # arbitrary
         "TxPower": dbus.Int16(0, variant_level=1),
         "ManufacturerData": dbus.Array([], signature="a{qv}", variant_level=1),
@@ -455,8 +457,8 @@ def AddDevice(self, adapter_device_name, device_address, alias):
     return path
 
 
-@dbus.service.method(BLUEZ_MOCK_IFACE, in_signature="ssi", out_signature="")
-def PairDevice(_self, adapter_device_name, device_address, class_):
+@dbus.service.method(BLUEZ_MOCK_IFACE, in_signature="ss", out_signature="")
+def PairDevice(_self, adapter_device_name, device_address):
     """Convenience method to mark an existing device as paired.
 
     You have to specify a device address which must be a valid Bluetooth
@@ -499,40 +501,14 @@ def PairDevice(_self, adapter_device_name, device_address, class_):
         "00001200-0000-1000-8000-00805f9b34fb",
     ]
 
-    device.props[DEVICE_IFACE]["UUIDs"] = dbus.Array(uuids, variant_level=1)
-    device.props[DEVICE_IFACE]["Paired"] = dbus.Boolean(True, variant_level=1)
-    device.props[DEVICE_IFACE]["LegacyPairing"] = dbus.Boolean(True, variant_level=1)
-    device.props[DEVICE_IFACE]["Blocked"] = dbus.Boolean(False, variant_level=1)
-
-    try:
-        device.props[DEVICE_IFACE]["Modalias"]
-    except KeyError:
-        device.AddProperties(
-            DEVICE_IFACE,
-            {
-                "Modalias": dbus.String("bluetooth:v000Fp1200d1436", variant_level=1),
-                "Class": dbus.UInt32(class_, variant_level=1),
-                "Icon": dbus.String("phone", variant_level=1),
-            },
-        )
-
-    device.EmitSignal(
-        dbus.PROPERTIES_IFACE,
-        "PropertiesChanged",
-        "sa{sv}as",
-        [
-            DEVICE_IFACE,
-            {
-                "UUIDs": dbus.Array(uuids, variant_level=1),
-                "Paired": dbus.Boolean(True, variant_level=1),
-                "LegacyPairing": dbus.Boolean(True, variant_level=1),
-                "Blocked": dbus.Boolean(False, variant_level=1),
-                "Modalias": dbus.String("bluetooth:v000Fp1200d1436", variant_level=1),
-                "Class": dbus.UInt32(class_, variant_level=1),
-                "Icon": dbus.String("phone", variant_level=1),
-            },
-            [],
-        ],
+    device.UpdateProperties(
+        DEVICE_IFACE,
+        {
+            "UUIDs": dbus.Array(uuids, variant_level=1),
+            "Paired": dbus.Boolean(True, variant_level=1),
+            "LegacyPairing": dbus.Boolean(True, variant_level=1),
+            "Blocked": dbus.Boolean(False, variant_level=1),
+        },
     )
 
 

tests/test_bluez5.py
@@ -180,7 +180,7 @@ class TestBlueZ5(dbusmock.DBusTestCase):
         self.assertEqual(path, "/org/bluez/" + adapter_name + "/dev_" + address.replace(":", "_"))
 
         # Pair with the device.
-        self.dbusmock_bluez.PairDevice(adapter_name, address, 5898764)
+        self.dbusmock_bluez.PairDevice(adapter_name, address)
 
         # Check the device's properties.
         out = "\n".join(_run_bluetoothctl("info " + address))

commit 97eadf3b1d20cec9aced62ce48eda47d5efcf40a

bluez5: Fix invalid arguments to PairDevice
The third (device class) argument to PairDevice was removed in 0.30.1,
but this call to it was still passing a third parameter, resulting in
an error from dbus-python whenever Pair() was called. This caused a
unit test regression in gnome-bluetooth.

Fixes: 63264e18 "bluez5: Clean up static default properties, re-drop PairDevice class_ parameter"
Resolves: https://github.com/martinpitt/python-dbusmock/issues/193
Bug-Debian: https://bugs.debian.org/1057564
Signed-off-by: Simon McVittie <smcv@debian.org>

dbusmock/templates/bluez5.py
@@ -333,7 +333,7 @@ def Pair(device):
         raise dbus.exceptions.DBusException("Device already paired", name="org.bluez.Error.AlreadyExists")
     device_address = device.props[DEVICE_IFACE]["Address"]
     adapter_device_name = Path(device.props[DEVICE_IFACE]["Adapter"]).name
-    device.PairDevice(adapter_device_name, device_address, MOCK_PHONE_CLASS)
+    device.PairDevice(adapter_device_name, device_address)
 
 
 @dbus.service.method(DEVICE_IFACE, in_signature="", out_signature="")