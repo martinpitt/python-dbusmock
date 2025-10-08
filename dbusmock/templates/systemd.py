f4eec96183992331facbac68cb326ed1fad00bba
Martin Pitt <martin@piware.de>
2025-10-08 07:40:47 +0200

    systemd template: Fix docstring for black 25.1.0
    Fixes #247

--- dbusmock/templates/systemd.py
+++ dbusmock/templates/systemd.py
@@ -1,5 +1,4 @@
-"""systemd mock template
-"""
+"""systemd mock template"""
 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
